82b333181b85261aa8462428f1b1328b27a83fc2
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-05-23 16:22:25 -0300

    adicionando SASS, criando estilização para os componentes

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import './style.scss'
 
-class Button extends React.Component{
-    render(){
-        return(
-            <button>
-                Botão
-            </button>
-        )
-    }
+class Button extends React.Component {
+  render() {
+    return (
+      <button className='botao'>
+        Botão
+      </button>
+    )
+  }
 }
 
 export default Button
