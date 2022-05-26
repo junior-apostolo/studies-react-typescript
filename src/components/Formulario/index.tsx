786819369b38d1f2aa875f24197c20b701eda2d5
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-05-26 12:46:05 -0300

    controlando formulario, adicionando tarefa

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { ITarefa } from "../../types/tarefa";
 import Button from '../Button'
 import style from './Form.module.scss'
 
 
-class Formulario extends React.Component {
+class Formulario extends React.Component <{
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}>{
+  state = {
+    tarefa: "",
+    tempo: "00:00"
+  }
+
+  adicionarTarefa(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, {...this.state}])
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">
             Adicione um novo estudo
@@ -14,6 +27,8 @@ class Formulario extends React.Component {
           <input 
             type="text" 
             name="tarefa"
+            value={this.state.tarefa}
+            onChange={(event) => this.setState({...this.state, tarefa: event.target.value})}
             id="tarefa"
             placeholder="O que voce quer estudar"
             required
@@ -27,13 +42,15 @@ class Formulario extends React.Component {
             type="time"
             step="1" 
             name="tempo"
+            value={this.state.tempo}
+            onChange={(event) => this.setState({...this.state, tempo: event.target.value})}
             id="tempo"
             min="00:00:00"
             max="01:30:00"
             required
             />
         </div>
-        <Button>
+        <Button type="submit">
           Adicionar
         </Button>
       </form>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Formulario from '../components/Formulario'
 import Lista  from '../components/Lista';
 import Timer from '../components/Timer';
+import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss'
 
 function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([])
   return (
     <div className={style.AppStyle}>
-      <Formulario />
-      <Lista />
+      <Formulario setTarefas={setTarefas}/>
+      <Lista tarefas={tarefas}/>
       <Timer/>
     </div>
   );
