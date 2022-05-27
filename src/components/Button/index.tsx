dfdb2a5add4b2dde74abadbce5305d4d9d5305af
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-05-27 14:40:40 -0300

    Refatorando class components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import style from './Button.module.scss'
 
+interface Props {
+  type?: "button" | "submit" | "reset" | undefined,
+  onClick?: () => void,
+  children: React.ReactNode
+}
 
-class Button extends React.Component<{type?: "button" | "submit" | "reset" | undefined, 
-onClick?: () => void,
-children: string
-}> {
-  render() {
-    const {type = "button", onClick} = this.props
-    return (
-      <button onClick={onClick} type={type} className={style.botao}>
-        {this.props.children}
-      </button>
-    )
-  }
+function Button({ onClick, type, children }: Props) {
+  return (
+    <button onClick={onClick} type={type} className={style.botao}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
