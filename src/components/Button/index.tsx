160402d9f143083723b1cc20d72287068a3e3b35
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-05-27 14:27:42 -0300

    mostrando relogio, criando regressiva para o relogio e finalizando tarefa

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import style from './Button.module.scss'
 
 
-class Button extends React.Component<{type?: "button" | "submit" | "reset" | undefined, children: string}> {
+class Button extends React.Component<{type?: "button" | "submit" | "reset" | undefined, 
+onClick?: () => void,
+children: string
+}> {
   render() {
-    const {type = "button"} = this.props
+    const {type = "button", onClick} = this.props
     return (
-      <button type={type} className={style.botao}>
+      <button onClick={onClick} type={type} className={style.botao}>
         {this.props.children}
       </button>
     )
--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -3,26 +3,39 @@ import Watch from "./Watch"
 import style from './Timer.module.scss'
 import { tempoParaSegundos } from "../../common/utils/time"
 import { ITarefa } from "../../types/tarefa"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 interface Props {
-  selecionado: ITarefa | undefined
+  selecionado: ITarefa | undefined,
+  finalizarTarefa: () => void
 }
 
-export default function Timer({selecionado}: Props){
+export default function Timer({selecionado, finalizarTarefa}: Props){
   const [tempo,setTempo] = useState<number>();
-  if(selecionado?.tempo){
-    setTempo(tempoParaSegundos(selecionado.tempo))
+
+  useEffect(() => {
+    if(selecionado?.tempo){
+      setTempo(tempoParaSegundos(selecionado.tempo))
+    }
+  },[selecionado])
+
+  function regressiva(contador: number = 0){
+    setTimeout(() => {
+      if(contador > 0){
+        setTempo(contador - 1)
+        return regressiva(contador - 1)
+      }
+      finalizarTarefa()
+    }, 1000)
   }
 
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronomentro</p>
-      Tempo: {tempo}
       <div className={style.relogioWrapper}>
-        <Watch/>
+        <Watch tempo={tempo}/>
       </div>
-      <Button>
+      <Button onClick={() => regressiva(tempo)}>
         Começar
       </Button>
     </div>
