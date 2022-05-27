9e4b468ec732d15911e8f5c26fe44c5b9f21a795
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-05-27 12:20:58 -0300

    iniciando cronometro, convertendo horas e minutos em segundos

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -1,11 +1,24 @@
 import Button from "../Button"
 import Watch from "./Watch"
 import style from './Timer.module.scss'
+import { tempoParaSegundos } from "../../common/utils/time"
+import { ITarefa } from "../../types/tarefa"
+import { useState } from "react"
 
-export default function Timer(){
+interface Props {
+  selecionado: ITarefa | undefined
+}
+
+export default function Timer({selecionado}: Props){
+  const [tempo,setTempo] = useState<number>();
+  if(selecionado?.tempo){
+    setTempo(tempoParaSegundos(selecionado.tempo))
+  }
+
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronomentro</p>
+      Tempo: {tempo}
       <div className={style.relogioWrapper}>
         <Watch/>
       </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -24,7 +24,7 @@ function App() {
       tarefas={tarefas}
       selecionaTarefa={selecionaTarefa}
       />
-      <Timer/>
+      <Timer selecionado={selecionado}/>
     </div>
   );
 }
